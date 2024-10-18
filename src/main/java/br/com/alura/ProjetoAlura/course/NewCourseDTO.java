4f8ab7f2287530fa0d067d005ae0509c66f45644
Gesley Elois <gesleyelois@gmail.com>
2024-10-18 10:05:21 -0300

    resolução

--- src/main/java/br/com/alura/ProjetoAlura/course/NewCourseDTO.java
+++ src/main/java/br/com/alura/ProjetoAlura/course/NewCourseDTO.java
@@ -3,6 +3,8 @@ package br.com.alura.ProjetoAlura.course;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 import org.hibernate.validator.constraints.Length;
 
 public class NewCourseDTO {
@@ -14,6 +16,7 @@ public class NewCourseDTO {
     @NotNull
     @NotBlank
     @Length(min = 4, max = 10)
+    @Pattern(regexp = "^[a-zA-Z]+(-[a-zA-Z]+)*$", message = "Código só pode conter letras e hífens para separar")
     private String code;
 
     private String description;
@@ -55,5 +58,9 @@ public class NewCourseDTO {
 
     public void setInstructorEmail(String instructorEmail) {
         this.instructorEmail = instructorEmail;
+    }
+    
+    public Course toModel() {
+    	return new Course(code, name, instructorEmail, description);
     }
 }
